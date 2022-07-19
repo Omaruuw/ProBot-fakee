61c320c9eeceb27ac76f9640adc9103be4894c77
SlayverB <100049719+SlayverB@users.noreply.github.com>
2022-07-19 15:15:55 +0200

    Delete commands/general directory

--- commands/general/daily.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'daily',
-  cooldown: 20,
-  async execute(message, args, client) {
-    message.channel.send("Daily has been moved to https://probot.io/daily");
-  },
-};
